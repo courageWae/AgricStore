45d9ee36517e806b30aabf871d903cf46ed183e1
Courage Ahorttor <cahorttor@toldoit.com>
2021-07-13 12:43:30 +0000

    intial commit

--- resources/views/BigStore/home.blade.php
+++ resources/views/BigStore/home.blade.php
@@ -1,145 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Agric Store</title>
-<!-- for-mobile-apps -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta property="og:title" content="Vide" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- //for-mobile-apps -->
-<link href="{{ asset('assets/css/bootstrap.css') }}" rel='stylesheet' type='text/css' />
-<!-- Custom Theme files -->
-<link href="{{ asset('assets/css/style.css') }}" rel='stylesheet' type='text/css' />
-<!-- js -->
-   <script src="{{ asset('assets/js/jquery-1.11.1.min.js') }}"></script>
-<!-- //js -->
-<!-- start-smoth-scrolling -->
-<script type="text/javascript" src="{{ asset('assets/js/move-top.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/easing.js') }}"></script>
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$(".scroll").click(function(event){
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-		});
-	});
-</script>
-<!-- start-smoth-scrolling -->
-<link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet">
-<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
-<!--- start-rate---->
-<script src="{{ asset('assets/js/jstarbox.js') }}"></script>
-	<link rel="stylesheet" href="{{ asset('assets/css/jstarbox.css') }}" type="text/css" media="screen" charset="utf-8" />
-		<script type="text/javascript">
-			jQuery(function() {
-			jQuery('.starbox').each(function() {
-				var starbox = jQuery(this);
-					starbox.starbox({
-					average: starbox.attr('data-start-value'),
-					changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
-					ghosting: starbox.hasClass('ghosting'),
-					autoUpdateAverage: starbox.hasClass('autoupdate'),
-					buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
-					stars: starbox.attr('data-star-count') || 5
-					}).bind('starbox-value-changed', function(event, value) {
-					if(starbox.hasClass('random')) {
-					var val = Math.random();
-					starbox.next().text(' '+val);
-					return val;
-					}
-				})
-			});
-		});
-		</script>
-<!---//End-rate---->
-
-</head>
+@include('layouts.main.script')
 <body>
 <a href="offer.html"><img src="{{ asset('assets/images/download.png') }}" class="img-head" alt=""></a>
-<div class="header">
 
-		<div class="container">
+<!-- HEADER SECTION -->
+@include('layouts.main.header')
 
-			<div class="logo">
-				<h1 ><a href="{{ route('home') }}"><b>T<br>H<br>E</b>Agric Store<span>The Best Agric Online Market</span></a></h1>
-			</div>
-			<div class="head-t">
-				<ul class="card">
-					<!-- <li><a href="wishlist.html" ><i class="fa fa-heart" aria-hidden="true"></i>Wishlist</a></li> -->
-					<li><a href="login.html" ><i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
-					<li><a href="register.html" ><i class="fa fa-arrow-right" aria-hidden="true"></i>Register</a></li>
-					<li><a href="about.html" ><i class="fa fa-file-text-o" aria-hidden="true"></i>Order History</a></li>
-					<!-- <li><a href="shipping.html" ><i class="fa fa-ship" aria-hidden="true"></i>Shipping</a></li> -->
-				</ul>
-			</div>
-
-			<!-- <div class="header-ri">
-				<ul class="social-top">
-					<li><a href="#" class="icon facebook"><i class="fa fa-facebook" aria-hidden="true"></i><span></span></a></li>
-					<li><a href="#" class="icon twitter"><i class="fa fa-twitter" aria-hidden="true"></i><span></span></a></li>
-					<li><a href="#" class="icon pinterest"><i class="fa fa-pinterest-p" aria-hidden="true"></i><span></span></a></li>
-					<li><a href="#" class="icon dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i><span></span></a></li>
-				</ul>
-			</div> -->
-
-
-				<div class="nav-top">
-					<nav class="navbar navbar-default">
-
-					<div class="navbar-header nav_2">
-						<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-
-
-					</div>
-					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-						<ul class="nav navbar-nav ">
-							<li class=" active"><a href="{{ route('home') }}" class="hyper "><span>Home</span></a></li>
-
-							<li class="dropdown ">
-								<a href="#" class="dropdown-toggle  hyper" ><span>Equipments</span></a>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle hyper"><span>Fertilizers</span></a>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle hyper" data-toggle="dropdown" ><span>Food Stuffs</span></a>
-							</li>
-							<!-- <li><a href="codes.html" class="hyper"> <span>Codes</span></a></li> -->
-							<li><a href="contact.html" class="hyper"><span>Contact Us</span></a></li>
-						</ul>
-					</div>
-					</nav>
-					 <div class="cart" >
-						<span class="fa fa-shopping-cart my-cart-icon"><span class="badge badge-notify my-cart-badge"></span></span>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-
-				</div>
-</div>
-  <!---->
-<div data-vide-bg="{{ asset('assets/video/video') }}">
-    <div class="container">
-		<div class="banner-info">
-			<h3>It is a long established fact that a reader will be distracted by
-			the readable </h3>
-			<div class="search-form">
-				<form action="#" method="post">
-					<input type="text" placeholder="Search..." name="Search...">
-					<input type="submit" value=" " >
-				</form>
-			</div>
-		</div>
-    </div>
-</div>
+  <!-- MAIN BANNER SECTION -->
+  @include('layouts.main.banner')
 
     <script>window.jQuery || document.write('<script src="{{ asset('assets/js/vendor/jquery-1.11.1.min.js') }}"><\/script>')</script>
     <script src="{{ asset('assets/js/jquery.vide.min.js') }}"></script>
@@ -190,31 +59,8 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--content-->
 
 
-  <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-         <a href="kitchen.html"> <img class="first-slide" src="{{ asset('assets/images/ba.jpg') }}" alt="First slide"></a>
-
-        </div>
-        <div class="item">
-         <a href="care.html"> <img class="second-slide " src="{{ asset('assets/images/ba1.jpg') }}" alt="Second slide"></a>
-
-        </div>
-        <div class="item">
-          <a href="hold.html"><img class="third-slide " src="{{ asset('assets/images/ba2.jpg') }}" alt="Third slide"></a>
-
-        </div>
-      </div>
-
-    </div><!-- /.carousel -->
+  <!-- Carousel -->
+  @include('layouts.main.carousel')
 
 <!--content-->
 	<div class="product">
@@ -409,76 +255,11 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 						 </div>
 		</div>
 	</div>
-<!--footer-->
-<div class="footer">
-	<div class="container">
-		<div class="col-md-3 footer-grid">
-			<h3>About Us</h3>
-			<p>Nam libero tempore, cum soluta nobis est eligendi
-				optio cumque nihil impedit quo minus id quod maxime
-				placeat facere possimus.</p>
-		</div>
-		<div class="col-md-3 footer-grid ">
-			<h3>Menu</h3>
-			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="kitchen.html">Kitchen</a></li>
-				<li><a href="care.html">Personal Care</a></li>
-				<li><a href="hold.html">Household</a></li>
-				<li><a href="codes.html">Short Codes</a></li>
-				<li><a href="contact.html">Contact</a></li>
-			</ul>
-		</div>
-		<div class="col-md-3 footer-grid ">
-			<h3>Customer Services</h3>
-			<ul>
-				<li><a href="shipping.html">Shipping</a></li>
-				<li><a href="terms.html">Terms & Conditions</a></li>
-				<li><a href="faqs.html">Faqs</a></li>
-				<li><a href="contact.html">Contact</a></li>
-				<li><a href="offer.html">Online Shopping</a></li>
 
-			</ul>
-		</div>
-		<div class="col-md-3 footer-grid">
-			<h3>My Account</h3>
-			<ul>
-				<li><a href="login.html">Login</a></li>
-				<li><a href="register.html">Register</a></li>
-				<li><a href="wishlist.html">Wishlist</a></li>
 
-			</ul>
-		</div>
-		<div class="clearfix"></div>
-			<div class="footer-bottom">
-				<h2 ><a href="index.html"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
-				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
-				<ul class="social-fo">
-					<li><a href="#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-					<li><a href="#" class=" twi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-					<li><a href="#" class=" pin"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-					<li><a href="#" class=" dri"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
-				</ul>
-				<div class=" address">
-					<div class="col-md-4 fo-grid1">
-							<p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
-					</div>
-					<div class="col-md-4 fo-grid1">
-							<p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>
-					</div>
-					<div class="col-md-4 fo-grid1">
-						<p><a href="mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
-					</div>
-					<div class="clearfix"></div>
 
-					</div>
-			</div>
-		<div class="copy-right">
-			<p> &copy; 2016 Big store. All Rights Reserved | Design by  <a href="http://w3layouts.com/"> W3layouts</a></p>
-		</div>
-	</div>
-</div>
-<!-- //footer-->
+<!-- footer -->
+@include('layouts.main.footer')
 
 <!-- smooth scrolling -->
 	<script type="text/javascript">
---
+++ resources/views/layouts/main/nav.blade.php
@@ -0,0 +1,34 @@
+<div class="nav-top">
+				<nav class="navbar navbar-default">
+
+					<div class="navbar-header nav_2">
+						<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+						<ul class="nav navbar-nav ">
+							<li class=" active"><a href="{{ route('home') }}" class="hyper "><span>Home</span></a></li>
+
+							<li class="dropdown ">
+								<a href="{{ route('equipments') }}" class="dropdown-toggle  hyper" ><span>Equipments</span></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle hyper"><span>Fertilizers</span></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle hyper" data-toggle="dropdown" ><span>Food Stuffs</span></a>
+							</li>
+							<!-- <li><a href="codes.html" class="hyper"> <span>Codes</span></a></li> -->
+							<li><a href="contact.html" class="hyper"><span>Contact Us</span></a></li>
+						</ul>
+					</div>
+				</nav>
+				<div class="cart" >
+					<span class="fa fa-shopping-cart my-cart-icon"><span class="badge badge-notify my-cart-badge"></span></span>
+				</div>
+				<div class="clearfix"></div>
+</div>
