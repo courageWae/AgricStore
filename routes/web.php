ba60e15ee52f5febdbacdb061b76fab1eb9ba274
Courage Ahorttor <cahorttor@toldoit.com>
2021-08-19 17:50:30 +0000

    [Web Route] codes refactored

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@ use App\Http\Controllers\InvoiceController;
 
 
 
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,10 +22,6 @@ use App\Http\Controllers\InvoiceController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', [BigStoreController::class, 'index'] )->name('index');
 Route::get('equipments', [ProductController::class, 'equipment'])->name('equipments');
@@ -52,10 +46,6 @@ Route::name('user.')->prefix('user-section')->group( function () {
 	Route::get('my_orders', [UserController::class, 'client_orders'])->name('orders');
 	Route::get('delete/my_orders/{user_product}', [UserProductController::class, 'destroy'])->name('product.delete');
 	Route::post('product/{user_product}/checkout', [UserController::class, 'product_checkout'])->name('checkout');  
-	//Route::get('plan/purchase/invoice/{category_alias}',[InvoiceController::class, 'show')->name('user.invoice');
-	// Route::get('appointments','UserController@appointments')->name('user.appointments');
-	// Route::get('package/{category_alias}','UserController@package_details')->name('user.viewPackage');
-	// Route::get('appointments/{booking}','UserController@view_appointment')->name('user.viewAppointment'); 
 });
 
 
@@ -66,15 +56,6 @@ Route::name('admin.')->prefix('admin-section')->group(function(){
 	Route::patch('profile/{admin}', [AdminController::class, 'update_profile'])->name('profile.update');
     Route::match(['get','post'], 'add/new_admin',[AdminController::class, 'add_admin'])->name('add');
 	Route::match(['get','post'], 'add/new/product', [ProductController::class, 'add_product'])->name('product.add');
-	// Route::match(['get','post'],'add/new/insurer','AdminController@add_insurer')->name('insurer.add');
-	// Route::get('create/project','ProjectController@showProjectForm')->name('project.show');
-	// Route::post('create/project','ProjectController@create_project')->name('project.create');
-	// Route::get('upload/photo','PhotoController@showPhotoUploadForm')->name('photo.show');
-	// Route::post('create/album','PhotoController@createAlbum')->name('create.album');
-	// Route::post('upload/photo','PhotoController@uploadPhoto')->name('photo.upload');
-	// Route::get('album/{album_alias}/view','PhotoController@albumPhotos')->name('view.album.photos');
-	// Route::get('photos/list','PhotoController@list_photo')->name('admin.list.photo');
-    // Route::get('photos/delete/{photo}','PhotoController@delete_photo')->name('admin.delete.photo');
 
 
 	Route::get('list/of/administrators', [AdminController::class, 'admin_list'])->name('list');
@@ -84,8 +65,6 @@ Route::name('admin.')->prefix('admin-section')->group(function(){
 	Route::get('list/of/clients',[AdminController::class, 'user_list'])->name('user.list');
 	Route::get('view/user/{client}',[AdminController::class, 'view_user'])->name('user.view');
 	Route::get('delete/user/{client}',[AdminController::class, 'delete_user'])->name('user.delete');
-	// Route::match(['get','post'],'add/type/of/lawyer','AdminController@type_of_lawyer_add')->name('type.lawyer.add');
-	// Route::get('delete/type/of/lawyer/{typeOfLawyer}','AdminController@delete_typeOfLawyer')->name('typeOfLawyer.delete');
 	Route::get('list/invoice',[InvoiceController::class, 'index'])->name('list.invoices');
 	Route::get('view/{invoice}/invoice',[InvoiceController::class, 'view'])->name('view.invoices');
 	Route::get('delete/{invoice}/invoice',[InvoiceController::class, 'delete'])->name('delete.invoices');
@@ -95,10 +74,6 @@ Route::name('admin.')->prefix('admin-section')->group(function(){
 	Route::patch('product/{product}/update', [ProductController::class, 'product_update'])->name('update.project');
 	Route::get('product/{product}/delete', [ProductController::class, 'product_delete'])->name('delete.project');
 });
-
-
-
-
 
 Auth::routes();
 
